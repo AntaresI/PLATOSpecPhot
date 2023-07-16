2a6125c4ed06fb9cccff914dc57b6d8ee3c0e59c
AntaresI <114310423+AntaresI@users.noreply.github.com>
2023-07-16 14:29:23 +0200

    Update Photometry.py

--- Photometry.py
+++ Photometry.py
@@ -1,156 +1,116 @@
 import numpy as np
-import photutils as pht
-from astropy.io import fits
-from photutils.utils import calc_total_error
 import os
-from photutils.aperture import CircularAnnulus, ApertureStats
+from pathlib import Path
+import configparser
 
-from Stars import getting_stars
+from Image_calibration import image_calibration
+from Photometry_parameters import phot_params_from_user
+from Photometry import LightCurve
+from Plotting import plotting
 from Centroid_shifting_and_LC_making import centroid_shifting_and_lc_making
+boolean_inputs = ["Y", "n"]
 
-def LightCurve(main_paths_obj_red, user_input_array, reduced_phot_path, reduced_lc_path):
-    
-    '''GETTING METHOD OF BACKGROUND COMPUTATION FROM USER'''
-    while True:
-        one_or_two = [1,2]
-        user_background_method_input = input("Write which method you wish to use as background computation."
-                                         " 1 corresponds to the iteration sigma method. 2 corresponds to"
-                                         " the annulus method. 1 or 2: ")
-    
-        user_background_method_input = int(user_background_method_input)
-        if user_background_method_input in one_or_two:
-            break
-        else:
-            print("Invalid input")
-    ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''   
-    '''IF BACKGROUND COMPUTATION METHOD IS THE ANNULUS METHOD THEN GETTING THE ANNULUS RADII FROM USER'''
-    if user_background_method_input == 2:
-        user_annulus_radii_input = input("Write the circular annulus inner and outer radius separated by"
-                                        " a comma which will be used to estimate the background \naround an"
-                                         " object. Note that the numbers you type in will be added to the"
-                                         " aperture radius (so if you type in 5,7 then \naperture radius + 5"
-                                         " will be used as a radius of the inner circular annulus and"
-                                         " aperture radius + 7 will be used as a \nradius for outer circular"
-                                        " annulus. Then press Enter: ")
-                                          
-        user_annulus_radii_input = user_annulus_radii_input.split(",")
-        annulus_radii = [int(float(param)) for param in user_annulus_radii_input]                                  
-    ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-    '''GETTING APERTURE RADII FROM USER'''
-    user_aperture_radii_input = input("Write the aperture radii that you wish to use for photometry"
-                                      " separated by a comma, then press Enter: ")
-                                       
-    user_aperture_radii_input = user_aperture_radii_input.split(",")
-    radii = [int(float(param)) for param in user_aperture_radii_input]
-    np.savetxt(reduced_lc_path.__str__()+'\\radii.txt',radii) 
-    ''''''''''''''''''''''''''''''''''''    
-        
-    '''FINDING ALL STARS IN SEQUENCE AND GATHERING INFO ABOUT THEIR POSITIONS AND MAGNITUDES'''
-    if user_background_method_input == 2:
-        for i,ifile in enumerate(main_paths_obj_red):
-          
-            print("aperture photometry :", i+1,"/",len(main_paths_obj_red),ifile)
-            
-            '''CREATING PHOT METADATA FILE AND LOADING IMAGE'''
-            rootname,_ = os.path.splitext(ifile)
-            photfile = str(reduced_phot_path)+"\\"+rootname.split("\\")[-1]+"-phot.fits"
-            data_img = fits.getdata(ifile)                                 
-            ''''''''''''''''''''''''''''''''''''''''''''''''''
-            
-            '''GETTING DETECTED STARS IN THE IMAGE'''
-            sources, bkg = getting_stars(user_input_array, data_img)
-            ''''''''''''''''''''''''''''''''''''''''''
-            
-            # https://photutils.readthedocs.io/en/stable/aperture.html#aperture-photometry-with
-            #-multiple-apertures-at-each-position
-          
-            '''GETTING POSITIONS OF CENTROIDS OF THE STARS'''
-            positions = [(ix,iy) for ix,iy in zip(sources['xcentroid'],sources['ycentroid'])]
-            apertures = [pht.CircularAperture(positions, r=r) for r in radii] 
-            ''''''''''''''''''''''''''''''''''''''''''''''''
-            
-            '''PERFORMING APERTURE PHOTOMETRY'''
-            error = calc_total_error(data_img-bkg.background, bkg.background_rms, 1/0.85)
-            aper_phot = pht.aperture_photometry(data_img, apertures, error=error)
-            ''''''''''''''''''''''''''''''''''''
-            print("Number of objects detected is: ", len(aper_phot))
-            print("X coordinates of centroids on the image are: ",aper_phot['xcenter']," Y coordinates of centroids on the image are: ",aper_phot['ycenter'])
-            
-            '''SUBTRACTING FLUX FROM CIRCULAR ANNULUS AND CONVERTING FLUX TO MAGNITUDE''' 
-            for j in range(len(radii)):
-                
-                annulus_aperture = CircularAnnulus(positions, r_in=radii[j]+annulus_radii[0], r_out=radii[j]+annulus_radii[1])
-                aperstats = ApertureStats(data_img, annulus_aperture)
-                annulus_bkg = apertures[j].area*aperstats.mean
-                
-                fcol = 'aperture_sum_'+str(j)
-                ecol = 'aperture_sum_err_'+str(j)
-                
-                flux = aper_phot[fcol]-annulus_bkg     
-                fluxerr = np.sqrt(aper_phot[ecol]**2+annulus_bkg) 
-                
-                mag = -2.5*np.log10(flux)
-                magerr = 2.5/(flux*np.log(10))*fluxerr
-                
-                aper_phot[fcol] = mag
-                aper_phot[ecol] = magerr
-                
-                aper_phot.rename_column(fcol,'mag_'+str(j))
-                aper_phot.rename_column(ecol,'magerr_'+str(j))
-                aper_phot.write(photfile,overwrite=True)
-            ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''   
-            
+'''ASKING WHETHER CALIBRATION OF IMAGES HAS ALREADY BEEN COMPLETED'''
+
+while True:
+    after_calibration_or_not = input("Has calibration of raw images already been completed? If yes,"
+                                   " the program will continue doing photometry with objects \nfound in"
+                                   " calib_obj directory of working directory (it is therefore expected"
+                                   " that calibration has been completed and \nthe working directory"
+                                   " structure is unchanged), if no then calibration will be performed."
+                                   " Y/n: ")
+    if after_calibration_or_not in boolean_inputs:
+        break
     else:
-        for i,ifile in enumerate(main_paths_obj_red):
-          
-            print("aperture photometry :", i+1,"/",len(main_paths_obj_red),ifile)
-            
-            '''CREATING PHOT METADATA FILE AND LOADING IMAGE'''
-            rootname,_ = os.path.splitext(ifile)
-            photfile = str(reduced_phot_path)+"\\"+rootname.split("\\")[-1]+"-phot.fits"
-            data_img = fits.getdata(ifile)                             
-            ''''''''''''''''''''''''''''''''''''''''''''''''''
-            
-            '''GETTING DETECTED STARS IN THE IMAGE'''
-            sources, bkg = getting_stars(user_input_array, data_img)
-            ''''''''''''''''''''''''''''''''''''''''''
-            
-            # https://photutils.readthedocs.io/en/stable/aperture.html#aperture-photometry-with-multiple-apertures-at-each-position
-            
-            '''GETTING POSITIONS OF CENTROIDS OF THE STARS'''
-            positions = [(ix,iy) for ix,iy in zip(sources['xcentroid'],sources['ycentroid'])]
-            apertures = [pht.CircularAperture(positions, r=r) for r in radii]  
-            ''''''''''''''''''''''''''''''''''''''''''''''''
-            
-            '''PERFORMING APERTURE PHOTOMETRY WITH SUBTRACTED BACKGROUND'''
-            error = calc_total_error(data_img-bkg.background, bkg.background_rms, 1/0.85)
-            aper_phot = pht.aperture_photometry(data_img-bkg.background, apertures, error=error)
-            print("Number of objects detected is: ", len(aper_phot))
-            print("X coordinates of centroids on the image are: ",aper_phot['xcenter']," Y coordinates of centroids on the image are: ",aper_phot['ycenter'])
-            ''''''''''''''''''''''''''''''''''''
-            
-            '''CONVERTING FLUX TO MAGNITUDE''' 
-            for j in range(len(radii)):
-                
-                fcol = 'aperture_sum_'+str(j)
-                ecol = 'aperture_sum_err_'+str(j)
-                
-                flux = aper_phot[fcol] 
-                fluxerr = aper_phot[ecol] 
-                
-                mag = -2.5*np.log10(flux)
-                magerr = 2.5/(flux*np.log(10))*fluxerr
-                
-                aper_phot[fcol] = mag
-                aper_phot[ecol] = magerr
-                
-                aper_phot.rename_column(fcol,'mag_'+str(j))
-                aper_phot.rename_column(ecol,'magerr_'+str(j))
-                aper_phot.write(photfile,overwrite=True)
-            ''''''''''''''''''''''''''''''''''''   
-            
-            
-    target_lc, comp_lc, vali_lc, radii = centroid_shifting_and_lc_making(main_paths_obj_red, radii)
+        print("Invalid input")
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''' 
+
+'''RUNNING CALIBRATION SCRIPT AND CREATING DIRECTORIES'''
+
+if after_calibration_or_not == "n":
+        
+        main_paths_obj_calib = image_calibration()
+        
+        config = configparser.ConfigParser()
+        config.read('config.ini')     
+        
+        working_dir = config['PATHS']['workingdirectory']
+        working_dir_path = Path(working_dir)
+        
+        reduced_lc_path = Path(working_dir +'//calibrated//Light_Curve')
+        reduced_lc_path.mkdir(exist_ok=True)
+        
+        reduced_phot_path = Path(working_dir +'//calibrated//reduced_obj_phot' )
+        reduced_phot_path.mkdir(exist_ok=True)
+
+      
+else:
+        print("LOADING CALIBRATED IMAGES")
+        config = configparser.ConfigParser()
+        config.read('config.ini')        
+        
+        working_dir = config['PATHS']['workingdirectory']
+        working_dir_path = Path(working_dir)
+        
+        reduced_lc_path = Path(working_dir +'//calibrated//Light_Curve')
+        reduced_lc_path.mkdir(exist_ok=True)
+        
+        calib_obj_path = Path(working_dir +'//calibrated//reduced_obj' )
+        calib_obj_path.mkdir(exist_ok=True)
+        
+        main_files_obj_calib = os.listdir(calib_obj_path)
+        main_paths_obj_calib = [calib_obj_path.__str__()+'//'+main_files_obj_calib[i] for i in range(len(main_files_obj_calib))]
+        
+        calib_phot_path = Path(working_dir +'//calibrated//reduced_obj_phot' )
+        calib_phot_path.mkdir(exist_ok=True)
+''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+'''ASKING WHETHER PHOTOMETRY HAS ALREADY BEEN COMPLETED'''
+
+while True:
+    after_phot_or_not = input("Has photometry already been completed? If yes the program will load"
+                              " the appropriate light curve data and will continue \non to matching and plotting,"
+                              " if no then photometry will be performed. Y/n: ")
+    if after_phot_or_not in boolean_inputs:
+        break
+    else:
+        print("Invalid input")
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+'''RUNNING PHOTOMETRY SCRIPTS'''    
     
-    return target_lc, comp_lc, vali_lc, radii
-    
+if after_phot_or_not == "n":
+           user_input_array = phot_params_from_user(main_paths_obj_calib)
+           targ_lc, comp_lc, vali_lc, radii = LightCurve(main_paths_obj_calib, user_input_array, calib_phot_path, reduced_lc_path)                                  
+
+else:
+           while True:
+               after_match_or_not = input("Has matching and light curve data generation already been"
+                                          " completed? If yes the program will load the appropriate"
+                                          " light curve data and will continue on to plotting, if no"
+                                          " then matching will be performed. Y/n: ")
+               if after_match_or_not in boolean_inputs:
+                   break
+               else:
+                   print("Invalid input")
+                   
+           if after_match_or_not == "n":
+                      radii = np.loadtxt(reduced_lc_path.__str__()+'//radii.txt')                     
+                      targ_lc, comp_lc, vali_lc, radii = centroid_shifting_and_lc_making(main_paths_obj_calib,radii)
+                      
+           else:
+                       
+                      targ_lc = np.loadtxt(reduced_lc_path.__str__()+'//target_lc.txt') 
+                      comp_lc = np.loadtxt(reduced_lc_path.__str__()+'//comp_lc.txt')
+                      vali_lc = np.loadtxt(reduced_lc_path.__str__()+'//vali_lc.txt')
+                      radii = np.loadtxt(reduced_lc_path.__str__()+'//radii.txt')
+''''''''''''''''''''''''''''''''
+
+
+'''PLOTTING LIGHT CURVE'''   
+       
+plotting(targ_lc, comp_lc, vali_lc, radii)
+''''''''''''''''''''''''''
+
+
+
